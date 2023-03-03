bf3af60a80c3382cec6d088d2c7b07349445b96d
hzc <3199917877@qq.com>
2023-03-03 22:42:26 +0800

    change dataset package

--- FSM3/few-shot-learning-FD-bearing-diff_type/options.py
+++ FSM3/few-shot-learning-FD-bearing-diff_type/options.py
@@ -5,7 +5,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='few-shot FD settings')
     parser.add_argument('--gpu_id', type=str, nargs='?', default='1', help="device id to train")
     parser.add_argument('--random_seed', default=10, type=int, help='random seed in experiments, default is 10')
-    parser.add_argument('--base_data_path', default='/data_disk/dataset/FD/bearing_data/2048/', help='base path keeping image data')
+    parser.add_argument('--base_data_path', default='/content/drive/MyDrive/Colab Notebooks/data_disk/dataset/FD/bearing_data/2048/', help='base path keeping image data')
 
     parser.add_argument('--exp_dataset', default='dataset_3', help='dataset_0/1/2/3')
     parser.add_argument('--split', default='target_outer', help='class10, target_4-7-10, target_ball, target_inner, target_outer')
@@ -15,8 +15,8 @@ def parse_args():
     parser.add_argument('--backbone_out_dim', default=100, type=int, help='feature dim')
     parser.add_argument('--in_channels', default=1, type=int, help='num of input channels')
 
-    parser.add_argument('--base_logdir', default='/data_disk/deeplearning/few-shot-learning-FD/exp_logs/', help='folder to keep all running files')
-    parser.add_argument('--method', default='proto_network_feat_space', help='finetune_last/finetune_whole/feature_knn/matching_network/matching_network_feat_space/matching_network_pretrain'
+    parser.add_argument('--base_logdir', default='/content/drive/MyDrive/Colab Notebooks/data_disk/deeplearning/few-shot-learning-FD/exp_logs/', help='folder to keep all running files')
+    parser.add_argument('--method', default='proto_network_feat_space', help='/content/drive/MyDrive/Colab Notebooks/finetune_last/finetune_whole/feature_knn/matching_network/matching_network_feat_space/matching_network_pretrain'
                                                                 '/feature_knn_proto/proto_network/proto_network_feat_space/proto_network_pretrain')
 
     # few-shot task setting
@@ -37,7 +37,7 @@ def parse_args():
     parser.add_argument('--pretrain_batchsize', default=16, type=int, help='batch size of pretrain epochs')
     parser.add_argument('--pretrain_save_freq', default=10, type=int, help='save interval')
 
-    parser.add_argument('--backbone_pretrain_path', default='', type=str, help='pretrain backbone path')  # /data_disk/deeplearning/few-shot-learning-FD/exp_logs/matching_network_pretrain/TRAIN/exp__003/saved_model/best_pretrain_backbone.pth
+    parser.add_argument('--backbone_pretrain_path', default='', type=str, help='pretrain backbone path')  # /content/drive/MyDrive/Colab Notebooks/data_disk/deeplearning/few-shot-learning-FD/exp_logs/matching_network_pretrain/TRAIN/exp__003/saved_model/best_pretrain_backbone.pth
 
     parser.add_argument('--pretrain_finetune_steps', default=100, type=int, help='pretrain_finetune_steps, for finetune whole model set to 100, for finetune last, set to 150')
 
